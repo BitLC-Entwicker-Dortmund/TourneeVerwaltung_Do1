5c0417d67453ab99b005c3c22b278bbc478acdc3
k s <klaus.stolz@web.de>
2022-11-16 18:58:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourneeVerwaltung_Do1 {
+    class Program {
+        static void Main ( string [ ] args ) {
+            Veranstaltung v = new Veranstaltung ( );
+
+            Mitarbeiter m1 = new Mitarbeiter ( );
+            Mitarbeiter m2 = new Mitarbeiter ( );
+            Mitarbeiter m3 = new Mitarbeiter ( );
+
+            v.AddMitarbeiter ( m1 );
+            v.AddMitarbeiter ( m2 );
+            v.AddMitarbeiter ( m3 );
+
+            v = null;
+        }
+    }
+}
+}
---
+++ Veranstaltung.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourneeVerwaltung_Do1 {
+	class Veranstaltung {
+		private Mitarbeiter [ ] mitarbeiter = new Mitarbeiter [ 3 ];
+		private int pos = 0;
+
+		public void SetMitarbeiter ( Mitarbeiter [ ] MAvonAussen ) {
+			mitarbeiter = MAvonAussen;
+		}
+
+		public Mitarbeiter [ ] GetMitarbeiter ( ) {
+			return mitarbeiter;
+		}
+
+		public void AddMitarbeiter ( Mitarbeiter m ) {
+			if ( pos < mitarbeiter.Length )
+				mitarbeiter [ pos ] = m;
+			pos++;
+		}
+
+
+		public Veranstaltung ( ) { }  // standardkonstruktor
+
+		public Veranstaltung ( int groesse ) {
+			mitarbeiter = new Mitarbeiter [ groesse ];
+		}
+	}
+}
